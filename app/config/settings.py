57963ffc36f824a9a11ea208f4131288d8102a74
guilherme096 <guilherme.rosa60@gmail.com>
2025-04-01 17:58:57 +0100

    change env names to fit azure standards; move binds to override

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
 
-GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLEMAPSAPIKEY")
+
