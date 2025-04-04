c33402004eb50c20dbcc72ef71c6fe5c1c549a08
João Roldão <jroldao04@ua.pt>
2025-04-04 16:16:14 +0100

    get photo endpoint done

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,3 +5,5 @@ load_dotenv()  # Load environment variables from .env file
 
 GOOGLE_MAPS_API_KEY = os.getenv("GOOGLEMAPSAPIKEY")
 
+if not GOOGLE_MAPS_API_KEY:
+    raise ValueError("GOOGLE_MAPS_API_KEY is not set in the environment variables.")
--- app/routes/places_router.py
+++ app/routes/places_router.py
@@ -4,6 +4,10 @@ from app.handlers.fetch_places_handler import fetch_places
 from app.models.fetch_places.request_models import PlacesRequest
 from app.models.fetch_places.response_models import PlacesResponse
 
+from app.handlers.get_photo_handler import get_photo
+from app.models.get_photos.request_models import Photo_gRPC
+from app.models.get_photos.response_models import Photo
+
 router = APIRouter(prefix="/places", tags=["places"])
 
 @router.post("/", response_model=PlacesResponse)
@@ -13,3 +17,11 @@ async def post_places(request: PlacesRequest):
     """
     places = await fetch_places(request)
     return PlacesResponse(places=places)
+
+@router.get("/photo", response_model=Photo)
+async def get_photo_endpoint(request: Photo_gRPC):
+    """
+    Fetch a photo using the POST method with a JSON request body.
+    """
+    photo = await get_photo(request)
+    return photo
